d2ede56d07b75e557023eaee654d6a25cfd60b35
zhur4k <kostya.zhurovich12052@gmail.com>
2024-12-08 11:12:40 +0300

    feat:complete task.

--- src/main/java/com/streamapi/Main.java
+++ src/main/java/com/streamapi/Main.java
@@ -1,7 +1,32 @@
 package com.streamapi;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        Создайте коллекцию студентов, где каждый студент содержит информацию о предметах, которые он изучает, и его оценках по этим предметам.
+        List<Student> students = Arrays.asList(
+                new Student("Student1", Map.of("Math", 90, "Physics", 85)),
+                new Student("Student2", Map.of("Math", 95, "Physics", 88)),
+                new Student("Student3", Map.of("Math", 88, "Chemistry", 92)),
+                new Student("Student4", Map.of("Physics", 78, "Chemistry", 85))
+        );
+
+//        Используйте Parallel Stream для обработки данных и создания Map, где ключ - предмет, а значение - средняя оценка по всем студентам.
+        var averageMarks = students.parallelStream()
+                .flatMap(student -> student.getGrades().entrySet().stream())
+                .collect(
+                        Collectors.groupingBy(
+                                Map.Entry::getKey,
+                                Collectors.averagingInt(Map.Entry::getValue)
+                        )
+                );
+
+//        Выведите результат: общую Map с средними оценками по всем предметам.
+            averageMarks.forEach((key, value) -> System.out.println(key + " : " + value));
     }
 }
---
+++ src/main/java/com/streamapi/Student.java
@@ -0,0 +1,17 @@
+package com.streamapi;
+
+import java.util.Map;
+
+public class Student {
+    private String name;
+    private Map<String, Integer> grades;
+
+    public Student(String name, Map<String, Integer> grades) {
+        this.name = name;
+        this.grades = grades;
+    }
+
+    public Map<String, Integer> getGrades() {
+        return grades;
+    }
+}
